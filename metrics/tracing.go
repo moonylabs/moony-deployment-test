24551b7f90facdcfafdc03f477e7b1b7919446d9
jeffyanta <jeff@flipcash.com>
2025-12-09 15:02:56 -0500

    Re-organize metrics package (#23)

--- metrics/tracing.go
+++ metrics/tracing.go
@@ -3,7 +3,38 @@ package metrics
 import (
 	"context"
 	"fmt"
+	"net/http"
 )
+
+// Trace represents an active trace that can contain multiple spans and attributes.
+type Trace interface {
+	// StartSpan starts a new span within the trace
+	StartSpan(name string) Span
+
+	// AddAttribute adds a key-value attribute to the trace
+	AddAttribute(key string, value interface{})
+
+	// OnError records an error on the trace
+	OnError(err error)
+
+	// SetRequest sets HTTP request information on the trace
+	SetRequest(r Request)
+
+	// SetResponse sets the HTTP response writer for the trace
+	SetResponse(w http.ResponseWriter) http.ResponseWriter
+
+	// End completes the trace
+	End()
+}
+
+// Span represents a timed span within a trace for tracing individual operations.
+type Span interface {
+	// AddAttribute adds a key-value attribute to the span
+	AddAttribute(key string, value interface{})
+
+	// End completes the span
+	End()
+}
 
 // TraceMethodCall traces a method call with a given struct/package and method names
 func TraceMethodCall(ctx context.Context, structOrPackageName, methodName string) *MethodTracer {
@@ -67,4 +98,21 @@ func (t *MethodTracer) End() {
 	}
 
 	t.span.End()
+}
+
+// traceContextKey is the context key for storing the current trace
+type traceContextKey struct{}
+
+// TraceKey is the context key for Trace
+var TraceKey = traceContextKey{}
+
+// NewContext returns a new context with the trace attached
+func NewContext(ctx context.Context, trace Trace) context.Context {
+	return context.WithValue(ctx, TraceKey, trace)
+}
+
+// TraceFromContext retrieves the trace from context, if present
+func TraceFromContext(ctx context.Context) Trace {
+	trace, _ := ctx.Value(TraceKey).(Trace)
+	return trace
 }
